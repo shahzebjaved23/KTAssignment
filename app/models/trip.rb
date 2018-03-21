9af93dbb5384192b40aa527656086a92bbdd599e
shahzebjaved23 <shahzebjaved23@gmail.com>
2018-03-22 03:29:02 +0500

    adds the trips specs

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,24 @@ class Trip < ApplicationRecord
 	belongs_to :truck
 
 	validates_presence_of :starting
+
+	validate :user_dosent_overlap,:truck_dosent_overlap
+
+	def user_dosent_overlap
+		users_other_trips = Trip.where(user_id: self.user_id) 
+		overlap_trips = users_other_trips.where("starting <= '#{self.starting.utc.iso8601}' AND ending >= '#{self.ending.utc.iso8601}'").or(users_other_trips.where("starting <= '#{self.starting.utc.iso8601}' AND ending IS NULL"))
+
+		if overlap_trips.count > 0
+			errors.add(:user_id, "user can't overlap with other trips")
+		end
+	end
+
+	def truck_dosent_overlap
+		trucks_other_trips = Trip.where(truck_id: self.truck_id) 
+		overlap_trips = trucks_other_trips.where("starting <= '#{self.starting.utc.iso8601}' AND ending >= '#{self.ending.utc.iso8601}'").or(trucks_other_trips.where("starting <= '#{self.starting.utc.iso8601}' AND ending IS NULL"))
+		
+		if overlap_trips.count > 0
+			errors.add(:truck_id, "truck can't overlap with other trips")
+		end
+	end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -9,9 +9,38 @@ RSpec.describe Trip do
 	end
 
 	it "cannot be created without a truck" do
-		user = User.create(last_name: "lastname", email: "email@some.com", password: "password")
+		user = User.create(first_name: "firstname",last_name: "lastname", email: "email@some.com", password: "password")
 		trip = Trip.create(user_id: user.id, starting: DateTime.now,ending: DateTime.now, distance: 232)
 		expect(trip.id).to be(nil)
+	end
+
+	it "cannot have truck with overlap trip" do
+		truck = Truck.create(number: "2424ln2l42")
+		truck2 = Truck.create(number: "2334343")
+
+		user = User.create(first_name: "firstname",last_name: "lastname", email: "email@some.com", password: "password")
+		trip = Trip.create(user_id: user.id, truck_id: truck.id, starting: DateTime.now - 5.days, ending: DateTime.now, distance: 232)
+		
+		trip2 = Trip.create(user_id: user.id, truck_id: truck2.id, starting: DateTime.now - 2.days, ending: DateTime.now)
+	
+		puts trip2.errors.messages.inspect if trip2.errors
+
+		expect(trip2.id).to be(nil)
+	end
+
+	it "cannot have user with overlap trip" do
+		truck = Truck.create(number: "2424ln2l42")
+		
+		user = User.create(first_name: "firstname",last_name: "lastname", email: "email@some.com", password: "password")
+		user2 = User.create(first_name: "firstname",last_name: "lastname", email: "email2@some.com", password: "password")
+		
+		trip = Trip.create(user_id: user.id, truck_id: truck.id, starting: DateTime.now - 5.days, ending: DateTime.now, distance: 232)
+		
+		trip2 = Trip.create(user_id: user2.id, truck_id: truck.id, starting: DateTime.now - 2.days, ending: DateTime.now)
+	
+		puts trip2.errors.messages.inspect if trip2.errors
+
+		expect(trip2.id).to be(nil)
 	end
 
 end
