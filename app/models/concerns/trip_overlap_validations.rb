58ce92ca0061ad0326bb8f0c112ed234fec7245a
shahzebjaved23 <shahzebjaved23@gmail.com>
2018-03-23 06:19:38 +0500

    refactors the trip validations

---
+++ app/models/concerns/trip_overlap_validations.rb
@@ -0,0 +1,86 @@
+module TripOverlapValidations
+
+	def self.included(base)
+  		base.class_eval do
+  			
+  			validate :starting_greater_than_ending, :validate_user_left_overlap, :validate_user_right_overlap, :validate_user_middle_overlap, :validate_user_outer_overlap, :validate_truck_left_overlap, :validate_truck_right_overlap, :validate_truck_middle_overlap, :validate_truck_outer_overlap
+
+  			def starting_greater_than_ending
+				if self.starting > self.ending
+					errors.add(:starting, "cannot be greater than ending")
+				end
+			end
+
+			# Validations for User Overlaps
+
+			def validate_user_left_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting <= '#{self.starting}' AND ending >= '#{self.starting}' AND ending <= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have left overlap with other trips")
+				end
+			end
+
+			def validate_user_right_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting >= '#{self.starting}' AND starting <= '#{self.ending}' AND ending >= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have right overlap with other trips")
+				end
+			end
+
+			def validate_user_middle_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting >= '#{self.starting}' AND ending <= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have middle overlap with other trips")
+				end
+			end
+
+			def validate_user_outer_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting <= '#{self.starting}' AND ending >= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have outer overlap with other trips")
+				end
+			end
+
+
+			# Validations for Truck Overlaps
+
+			def validate_truck_left_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting <= '#{self.starting}' AND ending >= '#{self.starting}' AND ending <= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have left overlap with other trips")
+				end
+			end
+
+			def validate_truck_right_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting >= '#{self.starting}' AND starting <= '#{self.ending}' AND ending >= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have right overlap with other trips")
+				end
+			end
+
+			def validate_truck_middle_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting >= '#{self.starting}' AND ending <= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have middle overlap with other trips")
+				end
+			end
+
+			def validate_truck_outer_overlap
+				overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting <= '#{self.starting}' AND ending >= '#{self.ending}'")
+
+				if overlap_trips.count > 0
+					errors.add(:user_id, "user can't have outer overlap with other trips")
+				end
+			end
+  		end
+	end
+
+	
+end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,7 @@
 class Trip < ApplicationRecord
 
+	include TripOverlapValidations
+
 	belongs_to :user
 	belongs_to :truck
 	belongs_to :company
@@ -8,81 +10,4 @@ class Trip < ApplicationRecord
 
 	validates_presence_of :starting
 
-	validate :starting_greater_than_ending, :validate_user_left_overlap, :validate_user_right_overlap, :validate_user_middle_overlap, :validate_user_outer_overlap, :validate_truck_left_overlap, :validate_truck_right_overlap, :validate_truck_middle_overlap, :validate_truck_outer_overlap
-
-
-	def starting_greater_than_ending
-		if self.starting > self.ending
-			errors.add(:starting, "cannot be greater than ending")
-		end
-	end
-
-	# Validations for User Overlaps
-
-	def validate_user_left_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting <= '#{self.starting}' AND ending >= '#{self.starting}' AND ending <= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have left overlap with other trips")
-		end
-	end
-
-	def validate_user_right_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting >= '#{self.starting}' AND starting <= '#{self.ending}' AND ending >= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have right overlap with other trips")
-		end
-	end
-
-	def validate_user_middle_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting >= '#{self.starting}' AND ending <= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have middle overlap with other trips")
-		end
-	end
-
-	def validate_user_outer_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(user_id: self.user_id).where("starting <= '#{self.starting}' AND ending >= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have outer overlap with other trips")
-		end
-	end
-
-
-	# Validations for Truck Overlaps
-
-	def validate_truck_left_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting <= '#{self.starting}' AND ending >= '#{self.starting}' AND ending <= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have left overlap with other trips")
-		end
-	end
-
-	def validate_truck_right_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting >= '#{self.starting}' AND starting <= '#{self.ending}' AND ending >= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have right overlap with other trips")
-		end
-	end
-
-	def validate_truck_middle_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting >= '#{self.starting}' AND ending <= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have middle overlap with other trips")
-		end
-	end
-
-	def validate_truck_outer_overlap
-		overlap_trips = Trip.where.not(id: self.id).where(truck_id: self.truck_id).where("starting <= '#{self.starting}' AND ending >= '#{self.ending}'")
-
-		if overlap_trips.count > 0
-			errors.add(:user_id, "user can't have outer overlap with other trips")
-		end
-	end
 end
